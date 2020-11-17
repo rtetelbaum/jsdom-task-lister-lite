e2f906e068dc9036533286a1b3c21207da5f74e5
rtetelbaum <rtetelbaum@mailbox.org>
2020-11-17 18:30:21 -0500

    lab done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	
+	/********** DOM Elements **********/
+	const taskForm = document.querySelector("#create-task-form")
+	const taskList = document.querySelector("#tasks")
+
+	/********** Event Listeners **********/
+	taskForm.addEventListener("submit", handleTaskFormSubmit)
+
+	/********** Event Handlers **********/
+	function handleTaskFormSubmit(event) {
+		event.preventDefault()
+
+		const taskDesc = event.target.querySelector("[name='new-task-description']").value
+
+		renderOneTask(taskDesc)
+
+		event.target.reset()
+	}
+
+	/********** Render Functions **********/
+	function renderOneTask(taskDesc) {
+		const task = document.createElement("li")
+		task.className = "task"
+		task.innerText = taskDesc
+		taskList.append(task)
+	}
+
 });
